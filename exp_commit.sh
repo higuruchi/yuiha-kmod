347365b18cd4f27ee7be221a6d1fc0597f6fea39
higuruchi <hfumiya0324@gmail.com>
2025-01-14 19:52:20 +0900

    Add functionality to archive experimental data before a user-specified date into a tar.gz file

--- exp_commit.sh
+++ exp_commit.sh
@@ -6,13 +6,25 @@ if [ ! -d "$TARGET_DIR" ]; then
 	exit 1
 fi
 
+before_day=${1:-3}
+limit_date=$(date --date="$before_day day ago" +"%Y-%m-%d")
+unix_limit_date=$(date -d "$limit_date" +%s)
+
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
 for dir in "$TARGET_DIR"/*; do
 	if [ -d "$dir" ]; then
 		dirname=$(basename "$dir")
-		tar -czf "$TARGET_DIR/${dirname}.tar.gz" "$dirname"
-		git add "$TARGET_DIR/${dirname}.tar.gz"
-		git commit -m "${dirname}"
-		git push origin main
-		rm -rf "$TARGET_DIR/$dirname"
+		exp_time=$(echo "$dirname" | sed 's/_.*//')
+		unix_exp_time=$(date -d "$exp_time" +%s)
+
+		if [ "$unix_exp_time" -le "$unix_limit_date" ]; then
+			echo $exp_time
+			tar -czf "${TARGET_DIR}/${dirname}.tar.gz" "${TARGET_DIR}/$dirname"
+			git add "${TARGET_DIR}/${dirname}.tar.gz"
+			git commit -m "${dirname}"
+			git push origin ${branch_name}
+			rm -rf "${TARGET_DIR}/${dirname}"
+		fi
 	fi
 done
